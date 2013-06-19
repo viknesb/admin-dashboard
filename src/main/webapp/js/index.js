4520b3150435d683c3b6f6d04e65114c6fdf1ccc
Viknes B <viknesb@gmail.com>
2013-06-18 21:28:32 -0400

    Refactoring and separating out controllers and services.

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,14 +1,14 @@
-var app = angular.module("adminMonitorApp",["encoder"]);
+var app = angular.module("adminMonitorApp",["controllers","services"]);
 
 app.config(['$httpProvider','$routeProvider' ,function($httpProvider, $routeProvider) {
 	$httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 	$routeProvider.
-	when('/', {controller:HomeCtrl, templateUrl:'home.html'}).
-	when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
-	when('/experiments', {controller:ExpCtrl, templateUrl:'experiments.html'}).
-	when('/projects', {controller:ProjectCtrl, templateUrl:'projects.html'}).
-	when('/workflows', {controller:WorkflowCtrl, templateUrl:'workflows.html'}).
+	when('/', {controller:'HomeCtrl', templateUrl:'home.html'}).
+	when('/edit/:projectId', {controller:'EditCtrl', templateUrl:'detail.html'}).
+	when('/experiments', {controller:'ExperimentCtrl', templateUrl:'experiments.html'}).
+	when('/projects', {controller:'ProjectCtrl', templateUrl:'projects.html'}).
+	when('/workflows', {controller:'WorkflowCtrl', templateUrl:'workflows.html'}).
 	otherwise({redirectTo:'/'});
 }]);
 
@@ -41,178 +41,198 @@ app.directive("adminboard", function() {
 	};
 });
 
-function HomeCtrl($scope, $http, Base64) {
 
+// Controllers
+angular.module("controllers",[]).
+	controller("HomeCtrl", ["$scope",function($scope) {
+		
+	}]).
+	controller("EditCtrl", ["$scope",function($scope) {
+		
+	}]).
+	controller("ExperimentCtrl", ["$scope","Experiment",function($scope,Experiment) {
+		Experiment.getAll().then(function(experiments) {
+			$scope.experiments = experiments;
+		});
+	}]).
+	controller("ProjectCtrl", ["$scope","Project",function($scope,Project) {
+		Project.getAll().then(function(projects) {
+			$scope.projects = projects;
+		});
+	}]).
+	controller("WorkflowCtrl", ["$scope",function($scope) {
+		
+	}]);
 
-}
-
-function EditCtrl($scope) {
-	
-}
-
-function ExpCtrl($scope, $http, Base64) {
-	
-	$http.defaults.headers.common.Authorization = make_base_auth("admin","admin", Base64);
-	$http({method:"GET", url:"http://localhost:8080/airavata-registry/api/experimentregistry/get/experiments/all",
-		cache : false}).
-	success(function(data,status) {
-		var getExperiments = function(data) {
-			var experiments = [];
-			for (var item in data.experiments) {
-				var experiment = {};
-				experiment.id = data.experiments[item].experimentId;
-				experiment.gatewayName = data.experiments[item].gateway.gatewayName;
-				experiment.projectName = data.experiments[item].project.projectName;
-				experiment.submittedDate = new Date(data.experiments[item].submittedDate).toLocaleString();
-				experiment.username = data.experiments[item].user.userName;
-				experiments.push(experiment);
+// Services
+angular.module("services",["user"]).
+	factory("Project",["$http","User", function($http, User) {
+		return {
+			getAll : function() {
+				$http.defaults.headers.common.Authorization = User.getAuthHeader("admin","admin");
+				return $http({method:"GET", url:"http://localhost:8080/airavata-registry/api/projectregistry/get/projects",
+					cache : false}).
+				then(function(response) {
+					var results = response.data.workspaceProjects;
+					var projects = [];
+					for (var item in results) {
+						var project = {};
+						project.gatewayName = results[item].gateway.gatewayName;
+						project.projectName = results[item].projectName;
+						project.username = results[item].airavataUser.userName;
+						projects.push(project);
+					}
+					return projects;
+				}, function(error) {
+					console.log("Error occured while fetching projects !");
+				});
 			}
-			return experiments;
 		};
-		$scope.experiments = getExperiments(data);
-	}).
-	error(function(data,status) {
-		console.log("Error fetching experiments data !");
-	});
-}
-
-function ProjectCtrl($scope, $http, Base64) {
-	
-	$http.defaults.headers.common.Authorization = make_base_auth("admin","admin", Base64);
-	$http({method:"GET", url:"http://localhost:8080/airavata-registry/api/projectregistry/get/projects",
-		cache : false}).
-	success(function(data,status) {
-		var getProjects = function(data) {
-			var projects = [];
-			for (var item in data.workspaceProjects) {
-				var project = {};
-				project.gatewayName = data.workspaceProjects[item].gateway.gatewayName;
-				project.projectName = data.workspaceProjects[item].projectName;
-				project.username = data.workspaceProjects[item].airavataUser.userName;
-				projects.push(project);
+	}]).
+	factory("Experiment",["$http","User", function($http, User) {
+		return {
+			getAll : function() {
+				$http.defaults.headers.common.Authorization = User.getAuthHeader("admin","admin");
+				return $http({method:"GET", url:"http://localhost:8080/airavata-registry/api/experimentregistry/get/experiments/all",
+					cache : false}).
+				then(function(response) {
+					var results = response.data.experiments;
+					var experiments = [];
+					for (var item in results) {
+						var experiment = {};
+						experiment.id = results[item].experimentId;
+						experiment.gatewayName = results[item].gateway.gatewayName;
+						experiment.projectName = results[item].project.projectName;
+						experiment.submittedDate = new Date(results[item].submittedDate).toLocaleString();
+						experiment.username = results[item].user.userName;
+						experiments.push(experiment);
+					}
+					return experiments;
+				}, function(error) {
+					console.log("Error occured while fetching experiments !");
+				});
 			}
-			return projects;
 		};
-		$scope.projects = getProjects(data);
-	}).
-	error(function(data,status) {
-		console.log("Error fetching projects data !");
-	});
-}
-
-function WorkflowCtrl($scope, $http, Base64) {
-	
-	$http.defaults.headers.common.Authorization = make_base_auth("admin","admin", Base64);
-	$http({method:"GET", url:"http://localhost:8080/airavata-registry/api/userwfregistry/get/workflows",
-		cache : false}).
-	success(function(data,status) {
-		console.log(data);
-		var getWorkflows = function(data) {
-			var workflows = [];
-			for (var item in data.workflows) {
-				var workflow = {};
-				workflow.id = data.experiments[item].experimentId;
-				workflow.gatewayName = data.experiments[item].gateway.gatewayName;
-				workflow.projectName = data.experiments[item].project.projectName;
-				workflow.submittedDate = new Date(data.experiments[item].submittedDate).toLocaleString();
-				workflow.username = data.experiments[item].user.userName;
-				workflows.push(workflow);
+	}]).
+	factory("Workflow",["$http","User", function($http, User) {
+		return {
+			getAll : function() {
+				$http.defaults.headers.common.Authorization = User.getAuthHeader("admin","admin");
+				return $http({method:"GET", url:"http://localhost:8080/airavata-registry/api/userwfregistry/get/workflows",
+					cache : false}).
+				then(function(response) {
+					var results = response.data.experiments;
+					var workflows = [];
+					for (var item in data.workflows) {
+						var workflow = {};
+						workflow.id = data.experiments[item].experimentId;
+						workflow.gatewayName = data.experiments[item].gateway.gatewayName;
+						workflow.projectName = data.experiments[item].project.projectName;
+						workflow.submittedDate = new Date(data.experiments[item].submittedDate).toLocaleString();
+						workflow.username = data.experiments[item].user.userName;
+						workflows.push(workflow);
+					}
+					return workflows;
+				}, function(error) {
+					console.log("Error occured while fetching experiments !");
+				});
 			}
-			return workflows;
 		};
-		$scope.experiments = getWorkflows(data);
-	}).
-	error(function(data,status) {
-		console.log("Error fetching experiments data !");
+	}]);
+
+// Utils
+angular.module("user",["encoder"]).
+factory("User",["Base64", function(Base64) {
+	return {
+		getAuthHeader : function(username,password) {
+			var token = username + ':' + password;
+			return "Basic " + Base64.encode(token);
+		}
+	};
+}]);
+
+angular.module("encoder",[]).
+	factory('Base64', function() {
+	    var keyStr = 'ABCDEFGHIJKLMNOP' +
+	        'QRSTUVWXYZabcdef' +
+	        'ghijklmnopqrstuv' +
+	        'wxyz0123456789+/' +
+	        '=';
+	    return {
+	        encode: function (input) {
+	            var output = "";
+	            var chr1, chr2, chr3 = "";
+	            var enc1, enc2, enc3, enc4 = "";
+	            var i = 0;
+	 
+	            do {
+	                chr1 = input.charCodeAt(i++);
+	                chr2 = input.charCodeAt(i++);
+	                chr3 = input.charCodeAt(i++);
+	 
+	                enc1 = chr1 >> 2;
+	                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+	                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+	                enc4 = chr3 & 63;
+	 
+	                if (isNaN(chr2)) {
+	                    enc3 = enc4 = 64;
+	                } else if (isNaN(chr3)) {
+	                    enc4 = 64;
+	                }
+	 
+	                output = output +
+	                    keyStr.charAt(enc1) +
+	                    keyStr.charAt(enc2) +
+	                    keyStr.charAt(enc3) +
+	                    keyStr.charAt(enc4);
+	                chr1 = chr2 = chr3 = "";
+	                enc1 = enc2 = enc3 = enc4 = "";
+	            } while (i < input.length);
+	 
+	            return output;
+	        },
+	 
+	        decode: function (input) {
+	            var output = "";
+	            var chr1, chr2, chr3 = "";
+	            var enc1, enc2, enc3, enc4 = "";
+	            var i = 0;
+	 
+	            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+	            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+	            if (base64test.exec(input)) {
+	                alert("There were invalid base64 characters in the input text.\n" +
+	                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+	                    "Expect errors in decoding.");
+	            }
+	            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+	 
+	            do {
+	                enc1 = keyStr.indexOf(input.charAt(i++));
+	                enc2 = keyStr.indexOf(input.charAt(i++));
+	                enc3 = keyStr.indexOf(input.charAt(i++));
+	                enc4 = keyStr.indexOf(input.charAt(i++));
+	 
+	                chr1 = (enc1 << 2) | (enc2 >> 4);
+	                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+	                chr3 = ((enc3 & 3) << 6) | enc4;
+	 
+	                output = output + String.fromCharCode(chr1);
+	 
+	                if (enc3 != 64) {
+	                    output = output + String.fromCharCode(chr2);
+	                }
+	                if (enc4 != 64) {
+	                    output = output + String.fromCharCode(chr3);
+	                }
+	 
+	                chr1 = chr2 = chr3 = "";
+	                enc1 = enc2 = enc3 = enc4 = "";
+	 
+	            } while (i < input.length);
+	 
+	            return output;
+	        }
+	    };
 	});
-}
-
-function make_base_auth(user, password, Base64) {
-	var token = user + ':' + password;
-	return "Basic " + Base64.encode(token);
-}
-
-angular.module("encoder",[]).factory('Base64', function() {
-    var keyStr = 'ABCDEFGHIJKLMNOP' +
-        'QRSTUVWXYZabcdef' +
-        'ghijklmnopqrstuv' +
-        'wxyz0123456789+/' +
-        '=';
-    return {
-        encode: function (input) {
-            var output = "";
-            var chr1, chr2, chr3 = "";
-            var enc1, enc2, enc3, enc4 = "";
-            var i = 0;
- 
-            do {
-                chr1 = input.charCodeAt(i++);
-                chr2 = input.charCodeAt(i++);
-                chr3 = input.charCodeAt(i++);
- 
-                enc1 = chr1 >> 2;
-                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                enc4 = chr3 & 63;
- 
-                if (isNaN(chr2)) {
-                    enc3 = enc4 = 64;
-                } else if (isNaN(chr3)) {
-                    enc4 = 64;
-                }
- 
-                output = output +
-                    keyStr.charAt(enc1) +
-                    keyStr.charAt(enc2) +
-                    keyStr.charAt(enc3) +
-                    keyStr.charAt(enc4);
-                chr1 = chr2 = chr3 = "";
-                enc1 = enc2 = enc3 = enc4 = "";
-            } while (i < input.length);
- 
-            return output;
-        },
- 
-        decode: function (input) {
-            var output = "";
-            var chr1, chr2, chr3 = "";
-            var enc1, enc2, enc3, enc4 = "";
-            var i = 0;
- 
-            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-            var base64test = /[^A-Za-z0-9\+\/\=]/g;
-            if (base64test.exec(input)) {
-                alert("There were invalid base64 characters in the input text.\n" +
-                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                    "Expect errors in decoding.");
-            }
-            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
- 
-            do {
-                enc1 = keyStr.indexOf(input.charAt(i++));
-                enc2 = keyStr.indexOf(input.charAt(i++));
-                enc3 = keyStr.indexOf(input.charAt(i++));
-                enc4 = keyStr.indexOf(input.charAt(i++));
- 
-                chr1 = (enc1 << 2) | (enc2 >> 4);
-                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                chr3 = ((enc3 & 3) << 6) | enc4;
- 
-                output = output + String.fromCharCode(chr1);
- 
-                if (enc3 != 64) {
-                    output = output + String.fromCharCode(chr2);
-                }
-                if (enc4 != 64) {
-                    output = output + String.fromCharCode(chr3);
-                }
- 
-                chr1 = chr2 = chr3 = "";
-                enc1 = enc2 = enc3 = enc4 = "";
- 
-            } while (i < input.length);
- 
-            return output;
-        }
-    };
-});
