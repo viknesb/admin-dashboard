fc7aaf3c1a95f482e698b659c0ebd4529fdd4db0
viknesb <viknesb@gmail.com>
2013-10-03 18:00:17 -0400

    Changing registry url according to 0.9

--- src/main/test/dashboardTest.js
+++ src/main/test/dashboardTest.js
@@ -34,7 +34,7 @@ describe('adminMonitorApp', function() {
       it('should be defined',
 	    inject(function($rootScope, $controller, $httpBackend) {
           var scope = $rootScope.$new();
-		  $httpBackend.when('GET', 'api/projectregistry/get/projects').respond({});
+		  $httpBackend.when('GET', 'projectregistry/get/projects').respond({"workspaceProjects":[{"projectName":"default","gateway":{"gatewayName":"default"},"airavataUser":{"userName":"blah user"}}]});
           var ctrl = $controller("ProjectCtrl", {$scope : scope, project : {}});
           expect(ctrl).toBeDefined();
       }));
@@ -52,10 +52,18 @@ describe('adminMonitorApp', function() {
   });
   
   describe('services', function() {
-    var project,experiment,workflow;
+    var project = null,experiment = null,workflow = null;
     beforeEach( function() {
 	  module('services');
-	  inject(function(Project,Experiment,Workflow) {
+	  inject(function(Project,Experiment,Workflow,$httpBackend) {
+	    //Mock Responses
+	    $httpBackend.when('GET', 'projectregistry/get/projects').respond({"workspaceProjects":[{"projectName":"default","gateway":{"gatewayName":"default"},"airavataUser":{"userName":"blah user"}}]});
+		$httpBackend.when('GET', 'userwfregistry/get/workflows').respond({"workflowList":[{"workflowName":"Dihedral-Optimization-Workflow","workflowGraph":"<xwf:workflow xwf:version=\"0.8\" xmlns:xwf=\"http://airavata.apache.org/xbaya/xwf\">\n  <xgr:graph xgr:version=\"0.8\" xgr:type=\"ws\" xmlns:xgr=\"http://airavata.apache.org/xbaya/graph\">\n    <xgr:id>Dihedral_Optimization_Workflow</xgr:id>\n    <xgr:name>Dihedral-Optimization-Workflow</xgr:name>\n    <xgr:description></xgr:description>\n    <xgr:metadata>\n      <appinfo xmlns=\"http://www.w3.org/2001/XMLSchema\">\n\n</appinfo>\n    </xgr:metadata>\n    <xgr:node xgr:type=\"ws\">\n      <xgr:id>Prepare_Model_Reference_Data_invoke</xgr:id>\n      79Lw4YPS9euH0pQ0HRJS1sv+/fv\r\nucFB/r\nqwlPEA8AAAAAAAAAAAAAwD8BQTwAAAAAAAAAAAAAAATxAAAAAAAAAAAAAAAQxAMAAAAAAAAAAAAA\r\nQBAPAAAAAAAAAAAAAACu3/8HEK5g4Oe7Z1UAAAAASUVORK5CYII=\r\n</xwf:image>\n</xwf:workflow>"}]});
+		$httpBackend.when('GET', 'provenanceregistry/get/experiment?experimentId=exp1').respond({"executionStatus":null,"user":"user blah","output":null,"experimentId":"blah tmpl","metadata":null,"workflowInstanceName":null,"workflowInstanceDataList":[{"workflowInstance":{"experimentId":"blah tmpl","workflowExecutionId":"blah tmpl","templateName":"tmplate"},"workflowInstanceStatus":{"executionStatus":"UNKNOWN","statusUpdateTime":1373576118000,"workflowInstance":{"experimentId":"blah tmpl","workflowExecutionId":"blah tmpl","templateName":"blah tmpl"}},"nodeDataList":[{"workflowInstanceNode":{"workflowInstance":{"experimentId":"blah tmpl","workflowExecutionId":"blah tmpl","templateName":"blah tmpl"},"nodeId":null,"originalNodeID":null,"executionIndex":0},"inputData":null,"outputData":null,"input":null,"output":null,"status":{"executionStatus":"UNKNOWN","statusUpdateTime":1379801737868,"workflowInstanceNode":{"workflowInstance":{"experimentId":"blah tmpl","workflowExecutionId":"blah tmpl","templateName":"blah tmpl"},"nodeId":null,"originalNodeID":null,"executionIndex":0}},"type":"UNKNOWN","experimentId":"blah exp","workflowExecutionId":"blah wrkflw","nodeId":null}]}],"lazyLoaded":false});
+		$httpBackend.when('GET', 'experimentregistry/get/experiments/all').respond({"experiments":[{"experimentId":"Blah 1","submittedDate":1373576118000,"user":{"userName":"user blah"},"project":{"projectName":"default","gateway":{"gatewayName":"default"},"airavataUser":{"userName":"user blah"}},"gateway":{"gatewayName":"default"}}]});
+		$httpBackend.when('GET', 'provenanceregistry/get/experiment/user?username=user1').respond({"experiments":[{"experimentId":"Blah 1","submittedDate":1373576118000,"user":{"userName":"user blah"},"project":{"projectName":"default","gateway":{"gatewayName":"default"},"airavataUser":{"userName":"user blah"}},"gateway":{"gatewayName":"default"}}]});
+		$httpBackend.when('GET', 'provenanceregistry/workflow/errors?experimentId=exp1&workflowInstanceId=wrkflw1').respond({"WorkflowExecutionErrorList":[{"@class":"org.apache.airavata.registry.api.workflow.NodeExecutionError","source":"NODE","errorTime":1378760979000,"errorCode":null,"errorMessage":"Standard output is empty.","errorDescription":"Standard output is empty.","errorLocation":null,"actionTaken":null,"errorReported":null,"errorReference":0,"experimentId":"blah exp","workflowInstanceId":"blah wrkflw"}]});
+		$httpBackend.when('GET', 'provenanceregistry/node/errors?experimentId=exp1&workflowInstanceId=wrkflw1&nodeId=node1').respond({"nodeExecutionErrorList":[{"@class":"org.apache.airavata.registry.api.workflow.NodeExecutionError","source":"NODE","errorTime":1378760979000,"errorCode":null,"errorMessage":"Standard output is empty.","errorDescription":"Standard output is empty.","errorLocation":null,"actionTaken":null,"errorReported":null,"errorReference":0,"experimentId":"blah exp","workflowInstanceId":"blah wrkflw","nodeId":"Prepare_Model_Reference_Data_invoke"}]});
 	    project = Project;
 		experiment = Experiment;
 		workflow = Workflow;
@@ -71,6 +79,10 @@ describe('adminMonitorApp', function() {
 	describe('Experiment Service', function() {
 	  it('should have an getAll function', function () { 
         expect(angular.isFunction(experiment.getAll)).toBe(true);
+      });
+	  it('should return a not response for getAll function', function () { 
+        var hi = experiment.getAll();
+		console.log(hi);
       });
 	  it('should have an getByUser function', function () { 
         expect(angular.isFunction(experiment.getByUser)).toBe(true);
@@ -97,7 +109,7 @@ describe('adminMonitorApp', function() {
   });
   
   describe('config', function() {
-    var user,server;
+    var user = null,server = null;
     beforeEach( function() {
 	  module('config');
 	  inject(function(User,Server) {
